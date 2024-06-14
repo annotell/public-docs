8f423e8c0a830b32a8687a22e917738ffad355f4
Oskar Olin <oskar.olin@annotell.com>
2024-06-14 11:47:04 +0200

    Land on quickstart

--- docs-src/src/pages/index.js
+++ docs-src/src/pages/index.js
@@ -61,7 +61,7 @@ function Home() {
                 "button button--outline button--secondary button--lg",
                 styles.getStarted
               )}
-              to={useBaseUrl("docs")}
+              to={useBaseUrl("docs/guide/quickstart")}
             >
               Get Started
             </Link>
